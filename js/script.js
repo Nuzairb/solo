ae9a544e23da41b36b0b35cbd921c830a9b4cf37
Nuzair Ibrahim <nuzairb@gmail.com>
2018-10-03 21:10:57 +0500

    Finished jquery

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 /*How To Use Jquery*/
-$('h1').hide(500).show(500);
+$(function() {
+    $('h1').hide(500).show(500);
 
+    $('#main').css({
+        color: 'red',
+        fontSize: 25
+    });
+
+    $('h1').click(function() {
+        $('.content').css({
+            color: 'blue',
+            fontSize: 50
+        });
+    });
+});
+
+$(window).on("load", function() {
+    alert("Window is loaded.");
+});
